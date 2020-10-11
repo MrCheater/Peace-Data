758a09ba386cdb25bf4e6532534cf8e332366133
Mr.Cool <kylin98@list.ru>
2020-10-11 14:57:15 +0300

    Update webserver.py

--- server/webserver.py
+++ server/webserver.py
@@ -15,7 +15,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         query_components = parse_qs(urlparse(self.path).query)
         example_value = str(query_components["example_value"][0])
-        test = build(example_value)
+        test = build([16,24,4,13,612,0.335577,50,1080,1920,14,11,34,4,True,194,1,1,0,1,1,1,0,0,0,0])
 
         response = BytesIO()
         response.write(test.encode())
