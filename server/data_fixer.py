3b59648c734a35a16f07b491c95329780544323a
Mr.Cool <kylin98@list.ru>
2020-10-11 12:55:29 +0300

    Add files via upload

--- server/data_fixer.py
+++ server/data_fixer.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import pandas as pd
 import numpy as np
 
@@ -15,22 +9,10 @@ class DataFixer:
     def get_fix(self, data):
 
         self.data = data
-        self.data = self.data.drop(['Номер магазина'], axis=1)
-        
-        cat_cols = [i for i in self.data.columns if i.startswith('категория')]
-        
-        for i in cat_cols: self.data[i] = self.data[i].apply(lambda x: str(x[:-1]).replace(',','.'))
-        self.data[cat_cols] = self.data[cat_cols].apply(pd.to_numeric)
-        
-        num_cols = [i for i in self.data.columns if self.data[i].dtype != 'object']
-        obj_cols = [i for i in self.data.columns if self.data[i].dtype != 'object']
         
         self.data = pd.get_dummies(self.data)
         
         return self.data
-
-
-# In[ ]:
 
 
 
